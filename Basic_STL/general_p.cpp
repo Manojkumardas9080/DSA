5c29ce4bc924ed16dc57935e228833610ea74a06
Manoj <manojkumardas8809@gmail.com>
2024-10-13 13:49:47 +0530

    N string, q queries print frequencry of that string

--- Basic_STL/general_p.cpp
+++ Basic_STL/general_p.cpp
@@ -2,34 +2,31 @@
 using namespace std;
 
 void print(map<int, string> &m){
-  m[2] = {"mkd"};
-  cout <<"size: "<< m.size()<<endl;
-  for(auto pr : m){
-    cout <<pr.first <<" "<<pr.second<<endl;
-  }
 
-
+    m[2] = {"mkd"};
+    cout <<"size: "<< m.size()<<endl;
+    for(auto pr : m){
+      cout <<pr.first <<" "<<pr.second<<endl;
+    }
 
 }
 
 int main(){
-  map<int, string> m;
-  m[11] = {"abc"};
-  m[50] = {"bcd"};
-  m[3] = {"acd"};
+    map<int, string> m;
+    m[11] = {"abc"};
+    m[50] = {"bcd"};
+    m[3] = {"acd"};
 
-  m.insert({4, "afg"});
+    m.insert({4, "afg"});
 
-  print(m);
-  cout <<"------------------------"<<endl;
+    print(m);
+    cout <<"------------------------"<<endl;
 
-  cout<<"size: "<<m.size()<<endl;
-  for(auto &pr :m){
-    cout<<pr.first<<" "<<pr.second<<endl;
-  }
+    cout<<"size: "<<m.size()<<endl;
+    for(auto &pr :m){
+      cout<<pr.first<<" "<<pr.second<<endl;
+    }
 
   
-
-
-return 0;
+   return 0;
 }
---
+++ Basic_STL/un_map.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main(){
+  
+
+  int n;
+  cin>>n;
+  unordered_map<string, int> m;
+
+  for(int i=0; i<n; ++i){
+    string s;
+    cin >>s;
+    m[s]++;
+  }
+  
+  int q;
+  cin >>q;
+  while(q--){    //this loop runs q itmes, each time decrements 1 each iteration, continue utill reaches zero 
+    string s;   
+    cin >>s;
+    cout<<m[s] <<endl; // This line looks up the string s in the unordered map m and prints its associated count. 
+                      //If s does not exist in m, it will default to 0 (since the map returns 0 for keys that are not found).
+  }
+
+return 0;
+}
